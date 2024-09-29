93af5d9c4e677cd3a640b485eea9224a797bbd5f
aman <amanpreetsinghjhiwant@outlook.com>
2024-09-29 21:40:37 +0530

    s

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-// Start server
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
